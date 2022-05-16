c6a4226354a93a987e8d53c20122380b88678761
andrew <ms.andriy@gmail.com>
2022-05-16 14:51:05 +0300

    Update project

--- src/com/andrew/Appl.java
+++ src/com/andrew/Appl.java
@@ -3,5 +3,6 @@ package com.andrew;
 public class Appl {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("My new program");
     }
 }
